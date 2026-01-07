a7707918f88b53c17db081656d26a327a66bfa3d
Willian Silva <willvrsilva@gmail.com>
2026-01-07 13:41:25 -0300

    components
    jsx

---
+++ ReactJS/2_FUNDAMENTO/src/App.jsx
@@ -0,0 +1,17 @@
+//CSS Style
+import "./App.css";
+// Components
+import FirstComponent from "./components/FirstComponent.jsx";
+import TemplateExpressions from "./components/TemplateExpressions.jsx";
+
+function App() {
+  return (
+    <div>
+      <h1>Meu app.jsx</h1>
+      <FirstComponent />
+      <TemplateExpressions />
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/2_FUNDAMENTO/src/components/TemplateExpressions.jsx
@@ -0,0 +1,19 @@
+const TemplateExpressions = () => {
+  const data = {
+    name: "Willian",
+    age: 32,
+    work: "Auditor",
+    country: "Brasil",
+  };
+
+  return (
+    <div>
+      <p>Olá seja bem-vindo, {data.name}</p>
+      <p>Você tem {data.age} anos de idade</p>
+      <p>Sua função no mercado de trabalho é {data.work}</p>
+      <p>Você mora no {data.country}</p>
+    </div>
+  );
+};
+
+export default TemplateExpressions;
