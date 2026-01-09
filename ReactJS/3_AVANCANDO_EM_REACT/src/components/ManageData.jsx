8575be46b06a78a1065a24edde1d85dea8d9e405
Willian Silva <willvrsilva@gmail.com>
2026-01-09 13:09:27 -0300

    useState
    treinando

--- ReactJS/3_AVANCANDO_EM_REACT/src/components/ManageData.jsx
+++ ReactJS/3_AVANCANDO_EM_REACT/src/components/ManageData.jsx
@@ -1,21 +1,40 @@
 import { useState } from "react";
 
 const ManageData = () => {
-  const [state, setState] = useState(0);
+    const [userData, setUserData] = useState(0)
+    const data = [
+        {
+            nome: 'Willian',
+            idade: 32,
+            nacionalidade: 'Brasileiro',
+            sexo: 'Masculino'
+        },
+        {
+            nome: 'Mathias',
+            idade: 18,
+            nacionalidade: 'Espanhol',
+            sexo: 'Masculino'
+        },
+        {
+            nome: 'Joana',
+            idade: 27,
+            nacionalidade: 'Chilena',
+            sexo: 'Feminino'
+        }
+    ]
 
-  return (
-    <>
-      <h2>Utilizando useState</h2>
-      <p>
-        Contagem: <strong>{state}</strong>
-      </p>
-      <button onClick={() => setState(state + 1)}>Clique para somar + 1</button>
-      <button onClick={() => setState(state - 1)}>
-        Clique para subtrair - 1
-      </button>
-      <button onClick={() => setState(0)}>Clique para voltar ao inicio</button>
-    </>
-  );
+
+
+
+  return <div>
+        <h3>Dados de usuarios</h3>
+        <p>Nome: {data[userData].nome}</p>
+        <p>Idade: {data[userData].idade}</p>
+        <p>Nacionalidade: {data[userData].nacionalidade}</p>
+        <p>Sexo: {data[userData].sexo}</p>
+        <button onClick={() => setUserData(userData + 1)}>Próximo usuario</button>
+        <button onClick={() => setUserData(userData - 1)}>Usuario anterior</button>
+  </div>;
 };
 
 export default ManageData;
