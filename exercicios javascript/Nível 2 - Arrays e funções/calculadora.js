cd9ecb5fc7ccdbb196ba70c43c44ba00c459069c
Willian Silva <willvrsilva@gmail.com>
2026-08-21 11:37:54 -0300

    Arrays e funções
    proibido pesquisar

---
+++ exercicios javascript/Nível 2 - Arrays e funções/calculadora.js
@@ -0,0 +1,24 @@
+function somar(a, b){
+    let somar = a + b
+    console.log(somar)
+}
+
+function subtrair(a, b){
+    let subtrair = a - b
+    console.log(subtrair)
+}
+
+function multiplicar(a, b){
+    let multiplicar = a * b
+    console.log(multiplicar)
+}
+
+function dividir(a, b){
+    let dividir = a / b
+    console.log(dividir)
+}
+
+somar(12, 89)
+subtrair(562, 98)
+multiplicar(2, 8)
+dividir(10, 2)
---
+++ exercicios javascript/Nível 2 - Arrays e funções/soma.js
@@ -0,0 +1,6 @@
+function somar(num1, num2) {
+  let soma = num1 + num2;
+  console.log(`A soma de ${num1} + ${num2} é ${soma}`);
+}
+
+somar(56, 23);
