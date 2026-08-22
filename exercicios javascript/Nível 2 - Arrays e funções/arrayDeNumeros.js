d3abe051d33669c553675f02e4187ce38fae4c64
Willian Silva <willvrsilva@gmail.com>
2026-08-22 09:56:21 -0300

    Array de números

---
+++ exercicios javascript/Nível 2 - Arrays e funções/arrayDeNumeros.js
@@ -0,0 +1,11 @@
+//Somando array de numeros
+
+let numeros = [2, 34, 21, 11, 10, 5, 2, 6];
+
+let soma = 0 
+
+for(let i = 0; i < numeros.length; i++){
+    soma += numeros[i]
+}
+
+console.log(soma)
