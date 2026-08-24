b6b8dcf61648c6f56b56688f8f8ed3c50bc37238
Willian Silva <willvrsilva@gmail.com>
2026-08-24 08:28:41 -0300

    metodos

---
+++ exercicios javascript/metodos.js
@@ -0,0 +1,15 @@
+let food = ["Rice", "Beans", "Hamburguer", "Bread", "Fish", "Pasta"];
+
+food.push("Potato");
+
+console.log(food)
+
+food.sort()
+
+console.log(food)
+
+food.shift()
+
+console.log(food)
+
+console.log(food.indexOf("Pasta"))
