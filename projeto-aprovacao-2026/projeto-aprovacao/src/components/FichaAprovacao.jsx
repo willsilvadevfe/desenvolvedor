64ec61b5badc970335ece655bc9bf0c66a94eb6d
Willian Silva <willvrsilva@gmail.com>
2026-09-12 09:50:28 -0300

    sweetalert config

--- projeto-aprovacao-2026/projeto-aprovacao/src/components/FichaAprovacao.jsx
+++ projeto-aprovacao-2026/projeto-aprovacao/src/components/FichaAprovacao.jsx
@@ -43,10 +43,20 @@ const FichaAprovacao = () => {
       Swal.fire({
         title: "Atenção!",
         html: "<p>Preencha todos os campos obrigatórios antes de aprovar.</p>",
-        icon: "error",
-        iconColor: "#ef4444",
+        icon: "warning",
+        iconColor: "#f36d00",
+
         confirmButtonText: "Tentar novamente",
         confirmButtonColor: "#2563eb",
+
+        customClass: {
+          popup: "swal-popup",
+          title: "swal-title",
+          htmlContainer: "swal-text",
+          confirmButton: "swal-confirm-button",
+        },
+
+        buttonsStyling: true,
       });
       return;
     }
@@ -70,18 +80,18 @@ const FichaAprovacao = () => {
 
   return (
     <div className="ficha-aprovacao">
-      <a className="exit-icon" href="/">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="48px"
-          viewBox="0 -960 960 960"
-          width="48px"
-          fill="#000000"
-        >
-          <path d="M180-120q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h299v60H180v600h299v60H180Zm486-185-43-43 102-102H360v-60h363L621-612l43-43 176 176-174 174Z" />
-        </svg>
-      </a>
       <div className="form">
+        <a className="exit-icon" href="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="48px"
+            viewBox="0 -960 960 960"
+            width="48px"
+            fill="#000000"
+          >
+            <path d="M180-120q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h299v60H180v600h299v60H180Zm486-185-43-43 102-102H360v-60h363L621-612l43-43 176 176-174 174Z" />
+          </svg>
+        </a>
         <h2>Formulário para aprovação</h2>
         <small>{config.titulo}</small>
 
@@ -203,12 +213,28 @@ const FichaAprovacao = () => {
               <p>{valores[campo.id]}</p>
             </div>
           ))}
-              <div className="visual">
-                <p>Visual: OK</p>
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
-                <p>NOK</p>
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
-              </div>
+          <div className="visual">
+            <p>Visual: OK</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#000"
+            >
+              <path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+            </svg>
+            <p>NOK</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#000"
+            >
+              <path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+            </svg>
+          </div>
         </div>
 
         <div className="div4">
